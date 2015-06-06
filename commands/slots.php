394c29c103adeb0d80e0a1167d61fb3996b40255
Shelby Smith <galaxybuster@users.noreply.github.com>
2015-06-06 10:12:12 -0700

    Slot machine balanced. Added daily.php to repo, a script that is scheduled to run from a cronjob.

--- commands/slots.php
+++ commands/slots.php
@@ -15,67 +15,76 @@
 
 		// Check that the sender is registered
 		$user = new User($senderId);
-		if ($user->isRegistered) {
-			// grab sender money
-			$senderMoney = $user->bones;
-		} else {
-			$response = "i don't think you're registered, i-if that's ok..";
-			return $response;
+		if (!$user->isRegistered) {
+			return "i don't think you're registered, i-if that's ok..";
 		}
 		
-		// Check that the gambler has enough to wager
-		if ($amount > 0) {
-			if (fmod($amount, 1) == 0) {
-				if ($senderMoney >= $amount) {
-					// pull that lever
-					/*
-					slotValues[0] - names as key, percent as value
-					slotValues[1] - payout multiplier for this panel
-					*/
-					
-					$slotValues = array(
-						array(":boutit:"=>0.9, ":zippy:"=>3.0, ":wario:"=>1.1, ":eyepop:"=>2, ":mega:"=>3),
-						array(":boutit:"=>1.33, ":zippy:"=>0.3, ":wario:"=>0, ":eyepop:"=>0.8, ":mega:"=>0.25));
-					/*
-					the score will be tallied like so:
-					
-					repeat 3
-						weighted random select the slot accoring to slotValues[1][i]
-					payout = wager * sum(slotValue[2][roll])
-
-					max payout: 4x wager
-					min payout: 0
-						
-					As you can see this system is quite simple, so there will be vulnerabilities
-					*/
-
-					$payout = 0;
-					$slotString = "| ";
-					for ($i = 0; $i < 3; $i++) {
-						$v = getRandomWeightedElement($slotValues[0]);
-						// $v is the emoiji name
-						$slotString .= $v . " | ";
-
-						// get that multiplier
-						$payout += $slotValues[1][$v];
-					}
-
-					$payout = $amount * $payout;
-
-					$response = $slotString . "\r\n" . "You get :bone:" . $payout . " in return";
-					return $response;
-				} else {
-					$response = "you don't have enough, don't gamble your fur away";
-					return $response;
-				}
-			} else {
-				$response = "p-please no decimals, i can't understand them...";
-				return $response;
-			}
-		} else {
-			$response = "p-please bet a positive amount...";
-			return $response;
+		// Check that its a positive bet
+		if ($amount <= 0) {
+			return "p-please bet a positive amount...";
 		}
+		// Check that its an integer
+		if (fmod($amount, 1) != 0) {
+			return "p-please no decimals, i can't understand them...";
+		}
+
+		// Make sure sender has enough
+		if ($user->bones < $amount) {
+			return "don't make me come after you tryin to gamble what you aint got";
+		}
+
+		// pull that lever
+
+		/*
+		slotValues[0] - names as key, percent as value
+		slotValues[1] - payout multiplier for this panel
+		*/
+		$slotValues = array(
+			array(":boutit:"=>1, ":zippy:"=>5, ":wario:"=>7, ":eyepop:"=>2.4, ":mega:"=>4),
+			array(":boutit:"=>1.33, ":zippy:"=>0.2, ":wario:"=>0, ":eyepop:"=>0.8, ":mega:"=>0.25));
+		
+		/*
+		the score will be tallied like so:
+		
+		repeat 3
+			weighted random select the slot accoring to slotValues[1][i]
+		payout = wager * sum(slotValue[2][roll])
+
+		max payout: 4x wager
+		min payout: 0
+			
+		As you can see this system is quite simple, so there will be vulnerabilities
+		*/
+
+		$payout = 0;
+		$slotString = "| ";
+		for ($i = 0; $i < 3; $i++) {
+			$v = getRandomWeightedElement($slotValues[0]);
+			// $v is the emoiji name
+			$slotString .= $v . " | ";
+
+			// get that multiplier
+			$payout += $slotValues[1][$v];
+		}
+
+		$payout = round($amount * $payout);
+
+		// Money has to come from somewhere, let's get it from the bank
+		$bank = new User('bank');
+		
+		$bg = $bank->give($amount);
+		$ut = $user->take($amount);
+		$bt = $bank->take($payout);
+		$ug = $user->give($payout);
+
+		// if that all went smooth
+		if ($bg && $ut && $bt && $ug) {
+			$response = $slotString . "\r\n" . "You get :bone:" . $payout . " in return. You have :bone:" .$user->bones . ".";
+			return $response;
+		} else {
+			return "sorry, i fricked up";
+		}
+		
 		
 
 	}
---
+++ daily.php
@@ -0,0 +1,151 @@
+<?php
+	/*
+	Daily tasks to be run
+	This script is run every day at 10am server time.
+
+	Things it should do...
+
+	-delete swack images older than 1 day
+	-say goodmorning to all the crew
+		-if its a holiday say holla
+		-check if user's birthday?
+	-instate a bonus for the day on some condition
+
+	*/
+
+	require_once("lib/config.class.php");
+
+	$greetingsList = array(
+		"goodmorning!",
+		"goodmorning braingale!",
+		"hello my companions..!",
+		"i-i'm not doin so hot today...",
+		"_*sigh*_...",
+		"i'm so happy to be alive!!",
+		"you'll gamble too much!",
+		"how is everyone today?",
+		"i'm so glad to be here with you all!",
+		"today is gonna be great",
+		"you know i'm just not feelin it today",
+		"it's a so-good day",
+		"you :boutit:? im :boutit::boutit:",
+		"i wanna learn some new tricks today!",
+		"i can't wait to learn some new things",
+		"i'm so glad to be your friend",
+		"it's today!",
+		"you all can POUND SAND",
+		"i know some of you are cat people _mushbuh_"
+	);
+	$greeting = $greetingsList[rand(0, sizeof($greetingsList))];
+	$text = $greeting."\r\n";
+
+	//place cleanup script here for daily clean up of image folder
+	$filesCleaned = cleanup(60*60*24); // 1 day
+	if ($filesCleaned > 0) {
+		$text .= "i cleaned up ".$filesCleaned." old ".ngettext("file", "files", $filesCleaned).", i hope that's ok...\r\n";
+	}
+
+	// check if holiday say happyday
+	$holiday = holiday(date("Y-m-d"));
+	if ($holiday != false) {
+		$text .= "oh uh happy ".$holiday." by the way\r\n";
+	}
+
+	// check if bonus say theres a bonus
+	$bonus = false;
+	if ($bonus != false) {
+		$text .= "also, i'm givin extra bones for BGMP today...";
+	}
+
+
+	// this is the part that actually posts to slack
+	$payload = array("channel"=>"#smartdog", "text"=>$text);
+	$fieldsAsString = "payload=".json_encode($payload);
+
+	// Initiate curl
+	$ch = curl_init();
+	// Disable SSL verification
+	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+	// Will return the response, if false it print the response
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	// configure as POST
+	curl_setopt($ch, CURLOPT_POST, 1);
+	// give it the post options
+	curl_setopt($ch, CURLOPT_POSTFIELDS, $fieldsAsString);
+	// Set the url
+	curl_setopt($ch, CURLOPT_URL, Config::get('slackIncomingURL')."?".$fieldsAsString);
+	// Execute
+	$data = curl_exec($ch);
+	// Closing
+	curl_close($ch);
+
+	echo $fieldsAsString;
+
+
+
+	// Utility functions for this script below.
+
+	// Determines if today is a holiday (defined in-script)
+	function holiday($date) {    
+		$year = substr($date, 0, 4); 
+
+		switch($date) {
+			case $year.'-01-01':
+				$holiday = 'New Year\'s Day';
+			break;
+			case date("Y-m-d", strtotime("-2 days", (easter_date($year)))):
+				$holiday = 'Good Friday';
+			break;
+			case date("Y-m-d", strtotime(easter_date($year))):
+				$holiday = 'Easter Sunday';
+			break;
+			case $year.'-06-05':
+				$holiday = 'Smartdog Day';
+			break;
+			case $year.'-07-04':
+				$holiday = 'US Independence Day';
+			break;
+			case date("Y-m-d", strtotime($year.'-09-00, first monday')):
+				$holiday = 'Labour Day';
+			break;
+			case date("Y-m-d", strtotime($year.'-11-00, fourth thursday')):
+				$holiday = 'Thanksgiving Day';
+			break;
+			case $year.'-12-24':
+				$holiday = 'Christmas Eve';
+			break;
+			case $year.'-12-25':
+				$holiday = 'Christmas';
+			break;
+			case $year.'-12-31':
+				$holiday = 'New Year\'s Eve';
+			break;
+			default:
+				$holiday = false;
+			break;
+		}
+
+		return $holiday;
+	}
+
+	// Cleans up temp files older than a period of time (in seconds)
+	// namely those created by commands/swack.php
+	// Returns number of files deleted
+	function cleanup($period) {
+		chdir("img");
+		$files = glob("*.png");
+		$now = time();
+
+		$i = 0;
+		foreach ($files as $file) {
+			if (is_file($file)) {
+				if ($now - filemtime($file) >= $period) {
+					if (unlink($file)) {
+						$i++;
+					}
+				}
+			}
+		}
+
+		return $i;
+	}
